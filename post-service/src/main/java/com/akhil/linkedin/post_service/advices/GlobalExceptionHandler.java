94bdac4e78aa211942ea8dac131ff689001dd4cb
Akhil Rajan <rajanakhil07@gmail.com>
2024-12-29 22:56:50 +0530

    add advices for GlobalExceptionHandler and GlobalResponseHandler

---
+++ post-service/src/main/java/com/akhil/linkedin/post_service/advices/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.akhil.linkedin.post_service.advices;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
