65328976257bbf0cbb57749c9c1fe0246db6be1e
Akhil Rajan <rajanakhil07@gmail.com>
2025-01-02 20:47:08 +0530

    add advices directory

---
+++ user-service/src/main/java/com/akhil/linkedin/user_service/advices/ApiError.java
@@ -0,0 +1,16 @@
+package com.akhil.linkedin.user_service.advices;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+
+@Data
+@Builder
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private Map<String, String> subErrors;
+}
---
+++ user-service/src/main/java/com/akhil/linkedin/user_service/advices/GlobalExceptionHandler.java
@@ -0,0 +1,74 @@
+package com.akhil.linkedin.user_service.advices;
+
+import com.akhil.linkedin.user_service.advices.ApiResponse;
+import com.akhil.linkedin.user_service.exceptions.EmailAlreadyInUseException;
+import com.akhil.linkedin.user_service.exceptions.ResourceNotFoundException;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<ApiResponse<?>> handleResourceNotFound(ResourceNotFoundException exception){
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(EmailAlreadyInUseException.class)
+    public ResponseEntity<ApiResponse<?>> handleEmailDuplicateException(EmailAlreadyInUseException exception){
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.CONFLICT)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ApiResponse<?>> handleBadRequestException(BadRequestException exception) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message(exception.getMessage())
+                .build();
+        return buildErrorResponseEntity(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<?>> handleValidationException(MethodArgumentNotValidException exception) {
+
+        Map<String, String> errors = new HashMap<>();
+
+        exception.getBindingResult().getFieldErrors().forEach(error ->
+                errors.put(error.getField(), error.getDefaultMessage()));
+
+        ApiError apiError = ApiError.builder()
+                .status(HttpStatus.BAD_REQUEST)
+                .message("Validation failed for one or more fields")
+                .subErrors(errors)
+                .build();
+        return buildErrorResponseEntity(apiError);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<?>> handleException(Exception exception) {
+        ApiError error = ApiError.builder()
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getLocalizedMessage())
+                .build();
+        return buildErrorResponseEntity(error);
+    }
+
+    public ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError error) {
+        return ResponseEntity.status(error.getStatus()).body(new ApiResponse<>(error));
+    }
+}
