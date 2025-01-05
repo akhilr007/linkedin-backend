a9b77eaf4f8df35c7318d275c2c8e21523973fb9
Akhil Rajan <rajanakhil07@gmail.com>
2025-01-05 22:07:41 +0530

    remove url from @RestController

--- connection-service/src/main/java/com/akhil/linkedin/connection_service/controllers/ConnectionController.java
+++ connection-service/src/main/java/com/akhil/linkedin/connection_service/controllers/ConnectionController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController("/core")
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class ConnectionController {
