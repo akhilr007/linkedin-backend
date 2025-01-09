68641e19cf2f867bf36af0410595dd4d4c193a06
Akhil Rajan <rajanakhil07@gmail.com>
2025-01-09 21:18:06 +0530

    add UserContextHeader and UserInterceptor

--- connection-service/src/main/java/com/akhil/linkedin/connection_service/controllers/ConnectionController.java
+++ connection-service/src/main/java/com/akhil/linkedin/connection_service/controllers/ConnectionController.java
@@ -1,5 +1,6 @@
 package com.akhil.linkedin.connection_service.controllers;
 
+import com.akhil.linkedin.connection_service.auth.UserContextHolder;
 import com.akhil.linkedin.connection_service.entities.Connection;
 import com.akhil.linkedin.connection_service.services.ConnectionService;
 import lombok.RequiredArgsConstructor;
@@ -16,18 +17,22 @@ public class ConnectionController {
 
     private final ConnectionService connectionService;
 
-    @GetMapping("/{userId}/first-degree")
-    ResponseEntity<List<Connection>> getFirstDegreeConnections(@PathVariable Long userId) {
+    @GetMapping("/first-degree")
+    ResponseEntity<List<Connection>> getFirstDegreeConnections() {
+        Long userId = UserContextHolder.getCurrentUserId();
         log.info("Received request for first-degree connections for userId: {}", userId);
+
         List<Connection> connections = connectionService.findFirstDegreeConnections(userId);
 
         log.info("Returning {} first-degree connections for userId: {}", connections.size(), userId);
         return ResponseEntity.ok(connections);
     }
 
-    @GetMapping("/{userId}/second-degree")
-    ResponseEntity<List<Connection>> getSecondDegreeConnections(@PathVariable Long userId) {
+    @GetMapping("/second-degree")
+    ResponseEntity<List<Connection>> getSecondDegreeConnections() {
+        Long userId = UserContextHolder.getCurrentUserId();
         log.info("Received request for second-degree connections for userId: {}", userId);
+
         List<Connection> connections = connectionService.findSecondDegreeConnections(userId);
 
         log.info("Returning {} second-degree connections for userId: {}", connections.size(), userId);
